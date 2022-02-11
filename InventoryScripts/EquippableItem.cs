c05a391ea4f17d3c2679e49fba4c63b22d40226a
Jeton245 <44135650+Jeton245@users.noreply.github.com>
2022-02-12 02:38:30 +0300

    Inventory

---
+++ InventoryScripts/EquippableItem.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+public enum EquipmentType
+{
+    Helmet,
+    Chest,
+    Gloves,
+    Boots,
+    Weapon1,
+    Weapon2,
+    Accessory1,
+    Accessory2,
+}
+[CreateAssetMenu]
+public class EquippableItem : Item
+{
+    public EquipmentType equipmentType;
+}
---
+++ InventoryScripts/Inventory.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] List<Item> items;
+    [SerializeField] Transform itemsParents;
+    [SerializeField] ItemSlot[] itemSlots;
+
+    private void OnValidate()
+    {
+        if (itemsParents != null)
+        {
+            itemSlots = itemsParents.GetComponentsInChildren<ItemSlot>();
+        }
+        RefreshUI();
+    }
+    private void RefreshUI()
+    {
+        int i = 0;
+        for (; i < items.Count && i < itemSlots.Length; i++)
+        {
+            itemSlots[i].Item = items[i];
+        }
+        for (; i < itemSlots.Length; i++)
+        {
+            itemSlots[i].Item = null;
+        }
+    }
+    public bool AddItem(Item item)
+    {
+        if (IsFull())
+        {
+            return false;
+        }
+            items.Add(item);
+            RefreshUI();
+            return true;
+        
+    }
+    public bool RemoveItem(Item item)
+    {
+        if (items.Remove(item))
+        {
+            RefreshUI();
+            return true;
+        }else
+        {
+            return false;
+        }
+        
+    }
+    public bool IsFull()
+    {
+        return items.Count >= itemSlots.Length;   
+    }
+}
